3c95fd0c6d69549fa2811a06f4d3a736cf5e70d3
caleb <caleb.schaefer@autonomalabs.com>
2025-03-30 15:26:30 -0400

    pre processing for 2d images

--- src/2d/datloader.py
+++ src/2d/datloader.py
@@ -3,7 +3,8 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, Dataset
 from PIL import Image
 import pandas as pd
-import os
+import os 
+from pretext_2d import rotate_2dimages
 
 
 #custom dataset class to load the training 
@@ -11,7 +12,7 @@ class Retinal2dDataset(Dataset):
     #@param takes the prediciton (csv file)
     #@param image_dir takes the images
     #@param transform, transforms each images to the specifies dimension
-    def __init__(self, preds_file, image_dir, transform=None):
+    def __init__(self, preds_file, image_dir, transform=None, task='rotate'):
         self.preds_file = preds_file
         self.image_dir = image_dir
         self.transform = transform
@@ -23,18 +24,39 @@ class Retinal2dDataset(Dataset):
         self.image_paths = self.df['id_code'].values + ".png"
         self.labels = self.df['diagnosis'].values
 
+        self.task = task
+        
+        self.images = []
+        self.rotated_labels = []
+
+        self.preprocess()
+
+
     def __len__(self):
         return len(self.image_paths)
 
+    #def preprocess transforms all the images and applies the nesseccary pre-text task
+    def preprocess(self):
+        for image_name in self.image_paths:
+            img_name = os.path.join(self.image_dir, image_name)
+            image = Image.open(img_name)
+            
+            if self.task == "rotate":
+                image, rotation_label = rotate_2dimages(image)
+            if self.transform:
+                image = self.transform(image)
+            
+            self.images.append(image)
+            self.rotated_labels.append(rotation_label)
+            
+
     def __getitem__(self, idx):
-        img_name = os.path.join(self.image_dir, self.image_paths[idx])
-        image = Image.open(img_name)
-        label = self.labels[idx]
+        img = self.images[idx]
+        label = None
+        if self.task == "rotate":
+            label = self.rotated_labels[idx]
 
-        if self.transform:
-            image = self.transform(image)
-
-        return image, label
+        return img, label
 
 
 #@def function returns a loaded image dataloader of the function
@@ -49,14 +71,11 @@ def load_2dimages():
     image_dataset = Retinal2dDataset(preds_file="/home/caleb/school/deep_learning/self-supervised-medical/dataset/2d/train.csv", 
         image_dir= "/home/caleb/school/deep_learning/self-supervised-medical/dataset/2d/train_images", transform=transform)
     
-    
     dataloader = DataLoader(image_dataset, batch_size=32, shuffle=True)
 
-    
-    
-    for images, _ in dataloader:
-        print(images.shape)
-        print(images.min(), images.max())
+    for image, label in dataloader:
+        print(image.shape)
+
 
 def main():
     load_2dimages()
--- src/2d/pretext_2d.py
+++ src/2d/pretext_2d.py
@@ -1,8 +1,17 @@
 import numpy as np
+from PIL import Image
+# @def rotates a single 2d image and and then returns the classifcation
+# the rotation classification for prediction
+# @param is a PIL image to be rotated
+def rotate_2dimages(image):
+    rotation_label = np.random.randint(low =1, high = 5)
+    rotation = rotation_label * 90
+    image = image.rotate(angle=rotation)
+
+    one_hot = np.zeros((4, 1))
+    one_hot[rotation_label -1] = 1
+
+    return image, one_hot
+    
 
 
-def pre_process_images(images)
-
-# Used to roate a group of 2d images together
-def rotate_2dimages(images, lables):
-    for images in 
