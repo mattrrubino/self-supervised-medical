2bcf7b842e913831676b6589cb24df2028c32b52
Matthew Rubino <mrrubino@buffalo.edu>
2025-03-30 13:08:16 -0400

    Add 3d rotation preprocessor

--- src/load.py
+++ src/load.py
@@ -1,6 +1,7 @@
 import nibabel as nib
 import numpy as np
-import skimage.transform as skTrans
+
+from pretext_3d import pretext_preprocess, rotation_preprocess
 
 
 def load_files(files: list[str]) -> np.ndarray:
@@ -8,51 +9,10 @@ def load_files(files: list[str]) -> np.ndarray:
     return np.array([img.get_fdata() for img in imgs]) # pyright: ignore
 
 
-def crop(data: np.ndarray, normalize: bool = True, threshold: float = 0.05) -> tuple[np.ndarray, tuple[int,int,int,int,int,int]]:
-    if normalize:
-        data = (data - data.min()) / (data.max() - data.min())
-
-    sx, ex, sy, ey, sz, ez = 0, 0, 0, 0, 0, 0
-    for x in range(data.shape[0]):
-        if np.any(data[x, :, :] > threshold):
-            sx = x
-            break
-    for x in range(data.shape[0] - 1, -1, -1):
-        if np.any(data[x, :, :] > threshold):
-            ex = x
-            break
-    for y in range(data.shape[1]):
-        if np.any(data[:, y, :] > threshold):
-            sy = y
-            break
-    for y in range(data.shape[1] - 1, -1, -1):
-        if np.any(data[:, y, :] > threshold):
-            ey = y
-            break
-    for z in range(data.shape[2]):
-        if np.any(data[:, :, z] > threshold):
-            sz = z
-            break
-    for z in range(data.shape[2] - 1, -1, -1):
-        if np.any(data[:, :, z] > threshold):
-            ez = z
-            break
-
-    return data[sx:ex,sy:ey,sz:ez], (sx,ex,sy,ey,sz,ez)
-
-
-def pretrain_preprocess(x: np.ndarray, y: np.ndarray, resolution=(128,128,128)) -> tuple[list[np.ndarray], list[np.ndarray]]:
-    assert len(x) == len(y),"Must have an equal number of inputs and outputs"
-    cropped_x = [crop(entry) for entry in x]
-    cropped_y = [label[sx:ex,sy:ey,sz:ez] for label,(_,(sx,ex,sy,ey,sz,ez)) in zip(y, cropped_x)]
-    print(cropped_x[0][0].shape, cropped_y[0].shape)
-    out_x = [skTrans.resize(entry, resolution, order=1, preserve_range=True) for entry,_ in cropped_x]
-    out_y = [skTrans.resize(entry, resolution, order=1, preserve_range=True) for entry in cropped_y]
-    print(out_x[0].shape, out_y[0].shape)
-    return out_x, out_y
-
-
 x = load_files(["../Task07_Pancreas/imagesTr/pancreas_001.nii.gz"])
 y = load_files(["../Task07_Pancreas/labelsTr/pancreas_001.nii.gz"])
-x, y = pretrain_preprocess(x, y)
+
+x, y = pretext_preprocess(x, y)
+x_rot, y_rot = rotation_preprocess(x)
+print(x_rot.shape, y_rot)
 
---
+++ src/pretext_3d.py
@@ -0,0 +1,80 @@
+import random
+
+import numpy as np
+import skimage.transform as skTrans
+
+
+def crop(data: np.ndarray, normalize: bool = True, threshold: float = 0.05) -> tuple[np.ndarray, tuple[int,int,int,int,int,int]]:
+    if normalize:
+        data = (data - data.min()) / (data.max() - data.min())
+
+    sx, ex, sy, ey, sz, ez = 0, 0, 0, 0, 0, 0
+    for x in range(data.shape[0]):
+        if np.any(data[x, :, :] > threshold):
+            sx = x
+            break
+    for x in range(data.shape[0] - 1, -1, -1):
+        if np.any(data[x, :, :] > threshold):
+            ex = x
+            break
+    for y in range(data.shape[1]):
+        if np.any(data[:, y, :] > threshold):
+            sy = y
+            break
+    for y in range(data.shape[1] - 1, -1, -1):
+        if np.any(data[:, y, :] > threshold):
+            ey = y
+            break
+    for z in range(data.shape[2]):
+        if np.any(data[:, :, z] > threshold):
+            sz = z
+            break
+    for z in range(data.shape[2] - 1, -1, -1):
+        if np.any(data[:, :, z] > threshold):
+            ez = z
+            break
+
+    return data[sx:ex,sy:ey,sz:ez], (sx,ex,sy,ey,sz,ez)
+
+
+def pretext_preprocess(x: np.ndarray, y: np.ndarray, resolution=(128,128,128)) -> tuple[np.ndarray, np.ndarray]:
+    assert len(x) == len(y),"Must have an equal number of inputs and outputs"
+    cropped_x = [crop(entry) for entry in x]
+    cropped_y = [label[sx:ex,sy:ey,sz:ez] for label,(_,(sx,ex,sy,ey,sz,ez)) in zip(y, cropped_x)]
+    out_x = [skTrans.resize(entry, resolution, order=1, preserve_range=True) for entry,_ in cropped_x]
+    out_y = [skTrans.resize(entry, resolution, order=1, preserve_range=True) for entry in cropped_y]
+    return np.array(out_x), np.array(out_y)
+
+
+def rotation_preprocess(x: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    b = x.shape[0]
+    y_rot = np.zeros((b, 10))
+
+    x_rot = []
+    for i, data in enumerate(x):
+        r = random.randrange(10)
+
+        if r == 0:
+            x_rot.append(data)
+        elif r == 1:
+            x_rot.append(np.transpose(np.flip(data, 1), (1, 0, 2)))
+        elif r == 2:
+            x_rot.append(np.flip(data, (0, 1)))
+        elif r == 3:
+            x_rot.append(np.flip(np.transpose(data, (1, 0, 2)), 1))
+        elif r == 4:
+            x_rot.append(np.transpose(np.flip(data, 1), (0, 2, 1)))
+        elif r == 5:
+            x_rot.append(np.flip(data, (1, 2)))
+        elif r == 6:
+            x_rot.append(np.flip(np.transpose(data, (0, 2, 1)), 1))
+        elif r == 7:
+            x_rot.append(np.transpose(np.flip(data, 0), (2, 1, 0)))
+        elif r == 8:
+            x_rot.append(np.flip(data, (0, 2)))
+        elif r == 9:
+            x_rot.append(np.flip(np.transpose(data, (2, 1, 0)), 0))
+        y_rot[i,r] = 1
+
+    return np.array(x_rot), y_rot
+
