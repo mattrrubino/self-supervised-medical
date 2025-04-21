d7253884d4d02d74ad896c9268497f3159c54c03
Matthew Rubino <mrrubino@buffalo.edu>
2025-04-21 09:03:49 -0400

    Add epoch graph

--- src/3d/graph.py
+++ src/3d/graph.py
@@ -3,6 +3,7 @@ import os
 import re
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 from train import RESULTS_PATH
 
@@ -12,7 +13,11 @@ if not os.path.exists(GRAPHS_PATH):
     os.makedirs(GRAPHS_PATH, exist_ok=True)
 
 
-def create_baseline_graph():
+def smooth(y, count=10):
+    return np.arange(0, len(y), count), [np.mean(y[i*count:(i+1)*count]) for i in range(len(y)//count)]
+
+
+def create_train_graph():
     results = [x for x in os.listdir(RESULTS_PATH) if "baseline_pancreas" in x]
     x = sorted([int(re.match(r"baseline_pancreas_(\d+).json", x).group(1)) for x in results]) # pyright: ignore
     y = []
@@ -23,15 +28,32 @@ def create_baseline_graph():
         dice = max([x["validation_dice"] for x in data])
         y.append(dice)
 
+    plt.figure(figsize=(15, 10))
     plt.plot(x, y, label="baseline", marker="o")
     plt.xticks(x)
-    plt.grid(True)
+    plt.grid()
     plt.xlabel("Percentage of Labeled Images", fontweight="bold")
     plt.ylabel("Average Dice Score", fontweight="bold")
     plt.legend(loc="lower right")
-    plt.savefig(os.path.join(GRAPHS_PATH, "baseline_pancreas.png"))
+    plt.savefig(os.path.join(GRAPHS_PATH, "train_pancreas.png"))
+
+
+def create_epoch_graph():
+    filepath = os.path.join(RESULTS_PATH, "baseline_pancreas_100.json")
+    with open(filepath) as f:
+        data = json.load(f)
+    x, y = smooth([d["validation_dice"] for d in data])
+
+    plt.figure(figsize=(15, 10))
+    plt.plot(x, y, label="baseline", marker="o")
+    plt.grid()
+    plt.xlabel("Epochs", fontweight="bold")
+    plt.ylabel("Average Dice Score", fontweight="bold")
+    plt.legend(loc="lower right")
+    plt.savefig(os.path.join(GRAPHS_PATH, "epoch_pancreas.png"))
 
 
 if __name__ == "__main__":
-    create_baseline_graph()
+    create_train_graph()
+    create_epoch_graph()
 
