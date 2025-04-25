f753ffa69bd744b7bbe4374495a54f1808a5fe98
Matthew Rubino <mrrubino@buffalo.edu>
2025-04-25 09:45:33 -0400

    Fix exemplar model

--- src/3d/train.py
+++ src/3d/train.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 import torch
+import torch.nn.functional as F
 
 from metrics import weighted_dice, weighted_dice_per_class
 
@@ -22,15 +23,19 @@ def epoch_text(epoch, prefix, l, m):
     return f"[EPOCH {epoch}] {prefix}: - loss {l:08.7f} - {metric_text}"
 
 
+def triplet_accuracy(a, p, n):
+    ap_dist = F.pairwise_distance(a, p)
+    an_dist = F.pairwise_distance(a, n)
+    return (ap_dist < an_dist).float().mean()
+
+
 def compute_metrics(preds, y, metrics):
-    if y is None:
-        return {}
     op = {
         "dice": weighted_dice,
         "dice_0": lambda a, b: weighted_dice_per_class(a, b, 0),
         "dice_1": lambda a, b: weighted_dice_per_class(a, b, 1),
         "dice_2": lambda a, b: weighted_dice_per_class(a, b, 2),
-        "accuracy": lambda a, b: (torch.argmax(a, dim=-1) == b).float().mean(),
+        "accuracy": lambda a, b: (torch.argmax(a, dim=-1) == b).float().mean() if b is not None else triplet_accuracy(*a),
     }
     return {metric: op[metric](preds, y).item() for metric in metrics}
 
