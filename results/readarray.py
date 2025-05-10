87717ca29a9365e0ea8426b81cac57e8da298f47
caleb <caleb.schaefer@autonomalabs.com>
2025-05-10 10:44:18 -0400

    moved results around

---
+++ results/readarray.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+rot = np.load("/home/caleb/school/deep_learning/self-supervised-medical/src/2d/graphs_and_data/kappa_scores_rotate.npy")
+jigsaw = np.load("/home/caleb/school/deep_learning/self-supervised-medical/src/2d/graphs_and_data/kappa_scores_jigsaw2.npy")
+base = np.load("/home/caleb/school/deep_learning/self-supervised-medical/src/2d/graphs_and_data/kappa_scores_base.npy")
+rpl = np.load("/home/caleb/school/deep_learning/self-supervised-medical/src/2d/graphs_and_data/kappa_scores_rpl.npy")
+exe = np.load("/home/caleb/school/deep_learning/self-supervised-medical/src/2d/graphs_and_data/kappa_scores_exe.npy")
+
+
+training_percent = [5, 10, 25, 50, 100]
+
+# Plot
+plt.figure(figsize=(6, 4))
+plt.plot(training_percent, base, label='baseline',  marker='o')
+plt.plot(training_percent, jigsaw, label='jigsaw', marker ='o') # Plot the first line with circle markers
+plt.plot(training_percent, rot, label='rotation', marker ='o') # Plot the first line with circle markers
+plt.plot(training_percent, rpl, label='rpl', marker ='o') # Plot the second line with square markers
+plt.plot(training_percent, exe, label='exemplar',  marker ='o', ) # Plot the second line with square markers
+plt.legend()
+plt.ylim(bottom=.5)
+
+plt.xticks(training_percent)
+plt.grid()
+plt.xlabel('Percentage of labeled images', fontweight="bold")
+plt.ylabel('Average validation kappa score', fontweight="bold")
+plt.grid(True)
+plt.show()
+
+
